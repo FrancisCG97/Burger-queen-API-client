1aa5a397a58a20903af9934bcf8076f540ad914e
Francis <f.carrasco.g.07@gmail.com>
2023-11-20 12:05:25 -0300

    Styles were modified

--- burger-queen-api-client/src/components/footer.jsx
+++ burger-queen-api-client/src/components/footer.jsx
@@ -1,5 +1,6 @@
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import burgerCrown from '../assets/Images/b-crown.png'
 
 function Footer() {
 
@@ -10,7 +11,7 @@ function Footer() {
         <div className="col-2 col-sm-8 col-md-6 col-lg-12 col-xl-12">
           <div className="container-fluid">
             <p id="footer" className="navbar">
-              {/* <img id="logo-footer" src={burgerCrown} alt="Logo" className="align-text-top"></img> */}
+              <img id="logo-footer" src={burgerCrown} alt="Logo" className="align-text-top"></img>
               Burger Queen
             </p>
           </div>
--- burger-queen-api-client/src/components/user.jsx
+++ burger-queen-api-client/src/components/user.jsx
@@ -9,15 +9,14 @@ function Users() {
             <br></br>
 
             <section id="login-user">
-
                 <div id="login-inputs">
                     <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
                         <label id="email" htmlFor="exampleFormControlInput1" className="form-label"> User </label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1"></input>
+                        <input type="email" className="form-control" placeholder="..." id="exampleFormControlInput1"></input>
                     </div>
                     <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
                         <label id="pass" htmlFor="inputPassword5" className="form-label"> Pass </label>
-                        <input type="password" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock"></input>
+                        <input type="password" id="inputPassword5" placeholder="..." className="form-control" aria-describedby="passwordHelpBlock"></input>
                     </div>
                 </div>
 
@@ -26,12 +25,9 @@ function Users() {
                         <div id="login">
                             <button type="button" className="btn"> Log in </button>
                         </div>
-                        <button type="button" className="btn" data-bs-toggle="modal" data-bs-target="#exampleModal"> New user <br></br> Admin only </button>
+                        <button type="button" className="btn"> New user <br></br> Admin only </button>
                     </div>
                 </div>
-
-
-
             </section>
 
         </>
